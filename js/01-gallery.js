43cb2de30e8520ad07b300d2a28e757e87b5a873
Dmytro Tarasenko <96738953+dmtarasenko@users.noreply.github.com>
2022-08-28 23:09:57 +0300

    task2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,12 +8,19 @@ galleryEl.addEventListener("click", onImageGalleryClick);
 galleryItemsCreator(galleryItems);
 
 function galleryItemsCreator(items) {
-  let gallaryMarkup = "";
-
-  gallaryMarkup = items
+  const gallaryMarkup = items
     .map(
       ({ preview, original, description }) =>
-        `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+              />
+            </a>
+        </div>`
     )
     .join("");
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("div.gallery");
+
+galleryItemsCreator(galleryItems);
+
+function galleryItemsCreator(items) {
+  const gallaryMarkup = items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"   
+                />
+            </a>`
+    )
+    .join("");
+
+  galleryEl.insertAdjacentHTML("beforeend", gallaryMarkup);
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+  caption: true,
+  nav: true,
+});
