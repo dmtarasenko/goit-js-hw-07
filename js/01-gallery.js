b520b419428cbc515f2358e26f57b19b3aa6e1a2
Dmytro Tarasenko <96738953+dmtarasenko@users.noreply.github.com>
2022-08-28 12:23:58 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("div.gallery");
+
+galleryEl.addEventListener("click", onImageGalleryClick);
+
+galleryItemsCreator(galleryItems);
+
+function galleryItemsCreator(items) {
+  let gallaryMarkup = "";
+
+  gallaryMarkup = items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`
+    )
+    .join("");
+
+  galleryEl.insertAdjacentHTML("beforeend", gallaryMarkup);
+}
+
+function onImageGalleryClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const originalImageLink = e.target.dataset.source;
+}
